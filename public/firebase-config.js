500f1154705d34853219ce5cd20b8aa6158aeb4f
amiyakumar07 <sahookumaramiya9@gmail.com>
2026-06-28 20:10:35 +0530

    Update Firebase project configuration credentials for project qrrprint

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,11 +1,12 @@
-// Firebase Configuration for PrintEase / QRPrint
+// Firebase Configuration for PrintEase / QRPrint (Project: qrrprint)
 const firebaseConfig = {
-  apiKey: "AIzaSyAAHCJU-p5KhVR_9_nePAIK8Q5Tnf5wOdk",
-  authDomain: "project-4bcf1ced-e226-40fa-b67.firebaseapp.com",
-  projectId: "project-4bcf1ced-e226-40fa-b67",
-  storageBucket: "project-4bcf1ced-e226-40fa-b67.firebasestorage.app",
-  messagingSenderId: "1004189609646",
-  appId: "1:1004189609646:web:544fb07bf760463539873f"
+  apiKey: "AIzaSyDWax18q8g-QJ05Acs0e_tz-0WEESt6KKs",
+  authDomain: "qrrprint.firebaseapp.com",
+  projectId: "qrrprint",
+  storageBucket: "qrrprint.firebasestorage.app",
+  messagingSenderId: "702154396525",
+  appId: "1:702154396525:web:dbf92975722cb6f924c064",
+  measurementId: "G-JXKSRBS7KE"
 };
 
 // Initialize Firebase
